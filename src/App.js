47f0cc96edcfafc4058f90f5a7ab057029a0ce5c
Farook <mohammedfarook085@gmail.com>
2023-11-23 22:34:01 +0530

    intial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+// import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <Cards />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Card/Card.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const Cards = () => {
+  return (
+    <div>
+      <p className="text-red-700">ff</p>
+    </div>
+  );
+};
